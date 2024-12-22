78b9eb3bfe74820239a02e60638ca6a493e0b8d8
Su Yang <soulteary@gmail.com>
2024-12-22 12:36:25 +0800

    refactor: split ftp

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	static "github.com/soulteary/gin-static"
 	"github.com/soulteary/ip-helper/model/define"
 	fn "github.com/soulteary/ip-helper/model/fn"
+	"github.com/soulteary/ip-helper/model/ftp"
 	ipInfo "github.com/soulteary/ip-helper/model/ip-info"
 	configParser "github.com/soulteary/ip-helper/model/parse-config"
 	"github.com/soulteary/ip-helper/model/response"
@@ -107,48 +108,6 @@ func handleTelnetConnection(ipdb *ipInfo.IPDB, conn net.Conn) {
 		fmt.Printf("发送消息时发生错误: %v\n", err)
 		return
 	}
-}
-
-func FTPServer(ipdb *ipInfo.IPDB) {
-	listener, err := net.Listen("tcp", ":21")
-	if err != nil {
-		log.Fatalf("Error creating server: %v", err)
-	}
-	defer listener.Close()
-
-	log.Println("FTP Server listening on :21")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Error accepting connection: %v", err)
-			continue
-		}
-		go handleFTPConnection(ipdb, conn)
-	}
-}
-
-func handleFTPConnection(ipdb *ipInfo.IPDB, conn net.Conn) {
-	defer conn.Close()
-
-	clientIP := fn.GetBaseIP(conn.RemoteAddr().String())
-	info := ipdb.FindByIPIP(clientIP)
-
-	sendBuf := [][]byte{}
-	message, err := json.Marshal(response.RenderJSON(clientIP, info))
-	if err != nil {
-		fmt.Println("序列化 JSON 数据时发生错误: ", err)
-		return
-	}
-	sendBuf = append(sendBuf, []byte("220"))
-	sendBuf = append(sendBuf, message)
-	sendBuf = append(sendBuf, []byte("\r\n"))
-	_, err = conn.Write(bytes.Join(sendBuf, []byte(" ")))
-	if err != nil {
-		log.Println("发送消息时发生错误: ", err)
-		return
-	}
-	conn.Close()
 }
 
 //go:embed public
@@ -269,7 +228,7 @@ func main() {
 	})
 
 	go TelnetServer(&ipdb)
-	go FTPServer(&ipdb)
+	go ftp.Server(&ipdb)
 
 	serverAddr := fmt.Sprintf(":%s", config.Port)
 	log.Printf("启动服务器于 %s:%s\n", config.Domain, config.Port)
